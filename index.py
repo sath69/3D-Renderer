2cba9f53ff7007136de0c094810a42e1718aae70
sath69 <98711640+sath69@users.noreply.github.com>
2025-11-08 16:15:15 +0000

    Update index.py
    GPU computation and kernel functions implemented

--- index.py
+++ index.py
@@ -6,12 +6,16 @@ import pyopencl as openCL
 import numpy as np
 
 code = """
-    __kernel void multiplyMat(__global float* input,__global float* matrix, __global float* output)
+    __kernel void multiplyMat(__global float4* vertices,__global float* matrix, __global float4* output)
     {
-        float x = input[0];
-        float y = input[1];
-        float z = input[2];
+        int i = get_global_id(0);
+        float4 v = vertices[i];
         
+        float x = v.x;
+        float y = v.y;
+        float z = v.z;
+        float w = v.w;
+
         float x_out = x*matrix[0] + y*matrix[4] + z*matrix[8] + matrix[12];
         float y_out = x*matrix[1] + y*matrix[5] + z*matrix[9] + matrix[13];
         float z_out = x*matrix[2] + y*matrix[6] + z*matrix[10] + matrix[14];
@@ -24,9 +28,7 @@ code = """
             z_out /= w_out;
         }
         
-        output[0] = x_out;
-        output[1] = y_out;
-        output[2] = z_out;
+        output[i] = (float4)(x_out, y_out,z_out,1.0f);
     }
 """
 
@@ -47,24 +49,21 @@ clock = pygame.time.Clock()
 start_ticks = pygame.time.get_ticks()
 FPS = 100 
 
-def multiplyMatrix(input, matrix):
+def multiplyMatrix(vertices, matrix):
       
-       _input = np.array([input.x, input.y, input.z,1], dtype=np.float32)
-       _matrix = np.array(matrix, dtype=np.float32).reshape(4,4)
-       _output = np.zeros(3,dtype=np.float32)
+       _num_vertices = vertices.shape[0]
+       _matrix = np.array(matrix, dtype=np.float32).reshape(16)
+       _output = np.zeros_like(vertices)
 
-       _inputBuffer = openCL.Buffer(context, openCL.mem_flags.READ_ONLY | openCL.mem_flags.COPY_HOST_PTR,hostbuf=_input)
+       _inputBuffer = openCL.Buffer(context, openCL.mem_flags.READ_ONLY | openCL.mem_flags.COPY_HOST_PTR,hostbuf= vertices)
        _matrixBuffer = openCL.Buffer(context, openCL.mem_flags.READ_ONLY | openCL.mem_flags.COPY_HOST_PTR,hostbuf=_matrix)
 
        _outputBuffer = openCL.Buffer(context, openCL.mem_flags.WRITE_ONLY, _output.nbytes)
 
        multiplyMat_kernel.set_args(_inputBuffer, _matrixBuffer, _outputBuffer)
-       openCL.enqueue_nd_range_kernel(queue, multiplyMat_kernel, (1,), None)
-
+       openCL.enqueue_nd_range_kernel(queue, multiplyMat_kernel, (_num_vertices,), None)
        openCL.enqueue_copy(queue, _output, _outputBuffer)
-      
-
-       return Vector3(_output[0],_output[1],_output[2])
+       return _output
 
 while True:
     for event in pygame.event.get():
@@ -126,42 +125,41 @@ while True:
        
     """
     #Render cube
+    verts = []
     for tri in cube.triangles:
+        
+        verts.extend([
+            [tri.v1.x, tri.v1.y, tri.v1.z, 1.0],
+            [tri.v2.x, tri.v2.y, tri.v2.z, 1.0],
+            [tri.v3.x, tri.v3.y, tri.v3.z, 1.0]])
+    verts = np.array(verts, dtype=np.float32)
+        
+    #Perform Z axis rotation
+    triRotZ = multiplyMatrix(verts, zRotMatrix)
+       
+    #Perform Y axis rotation
+    triRotZX = multiplyMatrix(triRotZ, xRotMatrix)
+       
+    triRotZX[:, 2] += 3.0
+    #Projection
+    triangleProjected = multiplyMatrix(triRotZX, projectionMatrix)
+       
+    #Scale the triangles into view
+    triangleProjected[:, 0] += 0.8
+    triangleProjected[:, 1] += 0.8
 
-        #Perform Z axis rotation
-        triRotZ1 = multiplyMatrix(tri.v1, zRotMatrix)
-        triRotZ2 = multiplyMatrix(tri.v2, zRotMatrix)
-        triRotZ3 = multiplyMatrix(tri.v3, zRotMatrix)
-       
-        #Perform Y axis rotation
-        triRotZX1 = multiplyMatrix(triRotZ1, xRotMatrix)
-        triRotZX2 = multiplyMatrix(triRotZ2, xRotMatrix)
-        triRotZX3 = multiplyMatrix(triRotZ3, xRotMatrix)
-       
-        #Projection
-        v1 = multiplyMatrix(triRotZX1.translate_z(triRotZX1,3), projectionMatrix)
-        v2 = multiplyMatrix(triRotZX2.translate_z(triRotZX2,3), projectionMatrix)
-        v3 = multiplyMatrix(triRotZX3.translate_z(triRotZX3,3), projectionMatrix)
-       
-        triangleProjected = Triangle(v1, v2, v3)
-       
-        #Scale the triangles into view
-        triangleProjected.v1.x += 0.8
-        triangleProjected.v1.y += 0.8
-        triangleProjected.v2.x += 0.8
-        triangleProjected.v2.y += 0.8
-        triangleProjected.v3.x += 0.8
-        triangleProjected.v3.y += 0.8
+    triangleProjected[:,0] *= 0.5 * screen.get_width() 
+    triangleProjected[:, 1] *= 0.5 * screen.get_height()
 
-        triangleProjected.v1.x *= 0.5 * screen.get_width() 
-        triangleProjected.v1.y *= 0.5 * screen.get_height()
-        triangleProjected.v2.x *=  0.5 * screen.get_width() 
-        triangleProjected.v2.y *= 0.5 * screen.get_height()
-        triangleProjected.v3.x *=  0.5 * screen.get_width() 
-        triangleProjected.v3.y *= 0.5 * screen.get_height()
-       
-        #Draw triangle
-        pygame.draw.polygon(screen, (0, 200, 255), [(int(triangleProjected.v1.x), int(triangleProjected.v1.y)), (int(triangleProjected.v2.x), int(triangleProjected.v2.y)), (int(triangleProjected.v3.x), int(triangleProjected.v3.y))], 2) 
+    #Draw Triangle 
+    for i in range(0, len(triangleProjected), 3):
+        vertices = [
+            (int(triangleProjected[i][0]), int(triangleProjected[i][1])),
+            (int(triangleProjected[i+1][0]), int(triangleProjected[i+1][1])),
+            (int(triangleProjected[i+2][0]), int(triangleProjected[i+2][1]))
+        ]
+        pygame.draw.polygon(screen, (0, 200, 255), vertices, 2)
+        
       
     pygame.display.flip()
     clock.tick(FPS)
